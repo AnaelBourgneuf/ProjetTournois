e0068a76192637fa20071c4aa2017033bda3b610
juliopopo1234 <ikaratchi@gmail.com>
2018-03-20 13:42:10 +0100

    srzfrzgr

--- event.php
+++ event.php
@@ -1,18 +1,19 @@
 <?php
 session_start();
-//$_SESSION["id"]="Anael";
 include "fonctions.php";
 $event = null;
 $contestants = null;
 $name = null;
 $creator = null;
 $time = null;
+$game=null;
 if (isset($_GET["id"])){
     $event = getEventById($_GET["id"]);
     $contestants = getEventContestantsId($_GET["id"]);
     $name = $event[0]["ev_name"];
     $creator = $event[0]["ev_creator"];
     $time = $event[0]["ev_stamp"];
+    $game = $event[0]["ev_game"];
 }
 ?>
 
@@ -57,76 +58,90 @@ if (isset($_GET["id"])){
 setMenu("");
 
 if (isset($_GET["id"])){
-    var_dump($event);
-
+    if (isset($_SESSION["id"])){
+        //var_dump($event);
+        echo '<div class="card-columns" id="form">
+        <div class="card">
+            ';//<img class="card-img-top" src="..." alt="Card image cap">
+            echo '<div class="card-body">
+                <h5 class="card-title">'.$game.'</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        <div class="card p-3">
+            <blockquote class="blockquote mb-0 card-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">
+                    <small class="text-muted">
+                        Someone famous in <cite title="Source Title">Source Title</cite>
+                    </small>
+                </footer>
+            </blockquote>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card bg-primary text-white text-center p-3">
+            <blockquote class="blockquote mb-0">
+                <p>'.$name.'</p>
+                <footer class="blockquote-footer text-white">
+                    <small class="text-white">
+                        Organisé par <cite title="Source Title"> '.$creator.'</cite>
+                    </small>
+                </footer>
+            </blockquote>
+        </div>
+        <div class="card text-center">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img" src="..." alt="Card image">
+        </div>
+        <div class="card p-3 text-right">
+            <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">
+                    <small class="text-muted">
+                        Someone famous in <cite title="Source Title">Source Title</cite>
+                    </small>
+                </footer>
+            </blockquote>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>';
+    }
+    else{
+        echo "<p id='form'>Vous devez vous <a href='connexion.php'><button class='btn btn-warning mr-sm-2' type ='button'><img src = 'images/profile.png' style = 'width: 2vw'> Connecter </button></a> pour accéder à un évennement</p>";
+    }
+    
+    
 }
+else{
+    echo "<div>
+             <h2>Ce Tournoi n'existe pas <a href='newEvent.php'>créez le !</a></h2>
+        </div>";
+
+ }
+
 ?>
 
 
-<div class="card-columns" id="form">
-    <div class="card">
-        <img class="card-img-top" src="..." alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">Card title that wraps to a new line</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-    </div>
-    <div class="card p-3">
-        <blockquote class="blockquote mb-0 card-body">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-            <footer class="blockquote-footer">
-                <small class="text-muted">
-                    Someone famous in <cite title="Source Title">Source Title</cite>
-                </small>
-            </footer>
-        </blockquote>
-    </div>
-    <div class="card">
-        <img class="card-img-top" src="..." alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-    </div>
-    <div class="card bg-primary text-white text-center p-3">
-        <blockquote class="blockquote mb-0">
-            <p><?php echo $name;?></p>
-            <footer class="blockquote-footer text-white">
-                <small class="text-white">
-                    Organisé par <cite title="Source Title"><?php echo $creator;?></cite>
-                </small>
-            </footer>
-        </blockquote>
-    </div>
-    <div class="card text-center">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-    </div>
-    <div class="card">
-        <img class="card-img" src="..." alt="Card image">
-    </div>
-    <div class="card p-3 text-right">
-        <blockquote class="blockquote mb-0">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-            <footer class="blockquote-footer">
-                <small class="text-muted">
-                    Someone famous in <cite title="Source Title">Source Title</cite>
-                </small>
-            </footer>
-        </blockquote>
-    </div>
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-    </div>
-</div>
+
 
 <?php
 setPub();
