320be41b878336a890c1032bab42d0317728b169
GinoX27 <ginox27@gmail.com>
2018-03-20 13:23:21 +0100

    A propos

--- aPropos.php
+++ aPropos.php
@@ -25,5 +25,14 @@ include "fonctions.php";
 <?php
 setMenu("Apropos");
 ?>
+<h1>À propos du site</h1>
+
+<p>Ce site permet (ou plutôt permetra vu l'état actuel) d'organiser des tournois ou des concours avec vos amis comme avec des inconus. Ceci sur les jeux de votre choix.
+Sur la page d'accueil vous pouvez voir tout les evennements prévus, en cours et passés. Vous pouvez acceder aux informations les concernant et aux profils des participants par des liens successifs.
+La force de ce site est son ouverture à n'importe quel jeu. Que vous voulier jouer en contre la montre pour voir qui fait le meilleur score ou vous affronter en 1V1, c'est possible.</p>
+
+<h1>Qui sommes nous ?</h1>
+
+<p>Nous (les créateurs) sommes une équipe de développeur en carton, en première année d'études d'informatique à l'IMIE de Caen. Pour notre projet nous devions mettre en place un site ou une application utilisant une base de donnée et le voici.</p>
 </body>
 </html>
