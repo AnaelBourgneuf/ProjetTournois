35cdcc007bf349c986a6db2d3a419e16ebf9bb5e
Bourgneuf-Monclair Anael <anael2011@hotmail.fr>
2018-03-20 18:04:27 +0100

    ajout prise en charge avatars

--- profil.php
+++ profil.php
@@ -22,18 +22,40 @@ include "fonctions.php";
         <title>SpearITournament - Profil</title>
 
         <style>
+            <?php
+            setMenuStyle();
+            setPubStyle();
+            setFormStyle();
+            ?>
+
             #disconnect {
                 float: right;
                 margin: 15px;
+                margin-right: 400px;
             }
         </style>
     </head>
     <body>
         <?php
         setMenu("Profil");
-        ?>
 
-        <a href="index.php?disconnect=TRUE" id="disconnect"><button type="button" class="btn btn-lg btn-outline-danger">Disconnect</button></a>
+        if (isset($_SESSION["id"])){
+            if (isset($_GET["id"])){
+                echo $_SESSION["id"]." - ".$_GET["id"];
+                if ($_SESSION["id"] == $_GET["id"]){
+                    echo "<a href=\"index.php?disconnect=TRUE\" id=\"disconnect\"><button type=\"button\" class=\"btn btn-lg btn-outline-danger\">Disconnect</button></a>";
+                }
+            }
+            else {
+                echo "<p id='form'>Aucun utilisateur n'est précisé, retournez à la page  <a href='index.php'><button class='btn btn-primary mr-sm-2' type ='button'> Accueil </button></a></p>";
+            }
+        }
+        else {
+            echo "<p id='form'>Vous devez vous <a href='connexion.php'><button class='btn btn-warning mr-sm-2' type ='button'><img src = 'images/profile.png' style = 'width: 2vw'> Connecter </button></a> pour accéder a un profil d'utilisteur</p>";
+        }
+
+        setPub();
+        ?>
 
     </body>
 </html>
