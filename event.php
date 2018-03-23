e663ab9fa13f0db34d98972573fb690da42113c3
Bourgneuf-Monclair Anael <anael2011@hotmail.fr>
2018-03-23 03:49:44 +0100

    maj

--- event.php
+++ event.php
@@ -12,6 +12,10 @@ if (isset($_GET["quit"])){
     deleteContestant($_SESSION["id"], $_GET["id"]);
 }
 
+if (isset($_POST)){
+    var_dump($_POST);
+}
+
 $event = null;
 $contestants = null;
 $name = null;
@@ -28,6 +32,7 @@ if (isset($_GET["id"])){
     $creator = $event[0]["ev_creator"];
     $time = $event[0]["ev_stamp"];
     $game = $event[0]["ev_game"];
+    $mode = $event[0]["ev_modal"];
     $nbmax=$event[0]["ev_cont_max"];
 }
 ?>
@@ -86,71 +91,81 @@ if (isset($_GET["id"])){
     if (isset($_SESSION["id"])){
         //var_dump($time);
         echo '<div class="card-columns" id="form">
-        <div class="card">
-            ';//<img class="card-img-top" src="..." alt="Card image cap">
-            echo '<div class="card-body">
-                <h5 class="card-title">'.$game.'</h5>
-                <p class="card-text">est un jeu vidéo de type arène de bataille en ligne (MOBA) gratuit développé et édité par Rito Gamezzz</p>
-            </div>
-        </div>
-        <div class="card p-3">
-            <blockquote class="blockquote mb-0 card-body">
-                <p>Mode de jeu :</p>
-                <footer class="blockquote-footer">
-                    <small class="text-muted">
-                        Co-op en équipe. <cite title="Source Title"></cite>
-                    </small>
-                </footer>
-            </blockquote>
-        </div>
-        <div class="card">
-            ';//<img class="card-img-top" src="..." alt="Card image cap">
-        echo '<div class="card-body">
-                <h5 class="card-title">Liste des Participants :</h5>
-                <p class="card-text">';
-                for ($i=0;$i<sizeof($contestants);$i++){
-                    echo $contestants[$i]["user_pseudo"];
-                }
-                echo '</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-        <div class="card bg-primary text-white text-center p-3">
-            <blockquote class="blockquote mb-0">
-                <p>'.$name.'</p>
-                <footer class="blockquote-footer text-white">
-                    <small class="text-white">
-                        Organisé par <cite title="Source Title"> '.$creator.'</cite>
-                    </small>
-                </footer>
-            </blockquote>
-        </div>
-        <div class="card text-center">
-            <div class="card-body">
-                <h5 class="card-title">Résultats :</h5>
-                <p class="card-text">BLABLABLA</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-        ';//<div class="card">
-            ;//<img class="card-img" src="..." alt="Card image">
-     //</div>
-     echo '<div class="card p-3 text-right">
-            <blockquote class="blockquote mb-0">
-                <p>Nombre maximum de participants : </p>
-                <footer class="blockquote-footer">
-                        '.$nbmax.'
-                </footer>
-            </blockquote>
-        </div>
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Date/heure :</h5>
-                <p class="card-text">'.$time.'</br></br>Année/Mois/Jour      Heure</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-    </div>';
+                <div class="card bg-primary text-white text-center p-3">
+                    <blockquote class="blockquote mb-0">
+                        <p>'.$name.'</p>
+                        <footer class="blockquote-footer text-white">
+                            <small class="text-white">
+                                Organisé par <cite title="Source Title"> '.$creator.'</cite>
+                            </small>
+                        </footer>
+                    </blockquote>
+                </div>
+                <div class="card">
+                    ';//<img class="card-img-top" src="..." alt="Card image cap">
+                    echo '<div class="card-body">
+                        <h5 class="card-title">'.$game.'</h5>
+                        <p class="card-text"></p>
+                    </div>
+                </div>
+                <div class="card p-3">
+                    <blockquote class="blockquote mb-0 card-body">
+                        <h5>Mode de jeu :</h5>
+                        <p class="card-text">'.$mode.'</p>
+                    </blockquote>
+                </div>
+                <div class="card">
+                    ';//<img class="card-img-top" src="..." alt="Card image cap">
+                echo '<div class="card-body">
+                        <h5 class="card-title">Liste des Participants :</h5>
+                        <p class="card-text">';
+                        for ($i=0;$i<sizeof($contestants);$i++){
+                            echo $contestants[$i]["user_pseudo"].", ";
+                        }
+                        echo '</p>
+                    </div>
+                </div>
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">Résultats :</h5>
+                            <div>';
+                //var_dump($contestants);
+                                for ($i = 0; $i < sizeof($contestants); $i++){
+                                    echo "<form class='card-text row' action='event.php?id=".$_GET["id"]."' method='post'> 
+                                            <div class='form mb-1 col' style='width: 10em;'>
+                                                <label for=\"pseudo\" class=\"sr-only\">".$contestants[$i]["user_pseudo"]."</label>
+                                                <input type=\"text\" readonly class=\"form-control-plaintext\" id=\"".$contestants[$i]["user_pseudo"]."\" name=\"".$contestants[$i]["user_pseudo"]."\" value=\"".$contestants[$i]["user_pseudo"]."\">
+                                            </div>
+                                            <div class=\"form mx-sm-3 mb-1 col\">
+                                                <label for=\"score\" class=\"sr-only\">".$contestants[$i]["cont_score"]."</label>
+                                                <input type=\"number\" class=\"form-control\" id=\"score".$contestants[$i]["user_pseudo"]."\" name=\"score".$contestants[$i]["user_pseudo"]."\" placeholder=\"".$contestants[$i]["cont_score"]."\">
+                                            </div>
+                                            <button type=\"submit\" class=\"btn btn-primary mb-2 col\">Envoyer</button>
+                                          </form>";
+                                }
+                            echo '</div>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                ';//<div class="card">
+                    ;//<img class="card-img" src="..." alt="Card image">
+             //</div>
+             echo '<div class="card p-3 text-right">
+                    <blockquote class="blockquote mb-0">
+                        <h5>Nombre maximum de participants : </h5>
+                        <footer class="blockquote-footer">
+                                '.$nbmax.'
+                        </footer>
+                    </blockquote>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Date/heure :</h5>
+                        <p class="card-text">'.$time.'</br></p>
+                        <p class="card-text"><small class="text-muted">Année-Mois-Jour Heure</small></p>
+                    </div>
+                </div>
+            </div>';
      //var_dump($_GET["id"]);
         $inscrit = false;
         for ($i = 0; $i < sizeof($contestants); $i++){
